6cecbf6b013dc7626b58d6025fbc30408bec295a
Nick Ray <nray@nray.io>
2020-02-07 16:26:13 -0700

    Add client files

---
+++ language/es5.js
@@ -0,0 +1,13 @@
+// "no-restricted-properties" rules are not properly merged when just using
+// "extends". Therefore we have to have this file which calls a custom merge
+// function. The merge function calls Object.assign with special handling for
+// configuration such as `no-restricted-properties` and `no-restricted-syntax`
+// which are array based - ensuring the two values being merged are concatenated
+const merge = require( 'eslint-config-wikimedia/language/merge.js' );
+const common = require('../common.json');
+const mergedRules = merge( common, require( 'eslint-config-wikimedia/language/not-es5.js' ) );
+
+module.exports = {
+	extends: "eslint-config-wikimedia/language/es5.json",
+	rules: mergedRules
+}
